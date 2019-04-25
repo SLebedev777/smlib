bade4a31387801898359e14bee1c388e8309c0d0
SLebedev777 <naotlichnenko@gmail.com>
2019-04-25 19:53:02 +0300

    started svd calculation

--- smlib/core/linalg.py
+++ smlib/core/linalg.py
@@ -10,4 +10,16 @@ import logging
 
 def svd(X):
     U, S, VT = np.linalg.svd(X)
+    assert len(X.shape)==2
+    m, n = X.shape
+    rank = min(m, n)
+    for iter in range(rank):
+        a = np.random.randn(n)
+        b = np.matmul(X, a) / np.linalg.norm(a)**2
+        for i in range(5000):
+            a = np.matmul(X.T, b) / np.linalg.norm(b)**2  # (n,)
+            b = np.matmul(X, a) / np.linalg.norm(a)**2  # (m,)
+            X1 = np.outer(b, a)
+            loss = np.linalg.norm(X - X1)**2
+            print(i, loss)
     return U, S, VT
