0e35b9392740cbd9665b89e17afb1df0958c88a5
SLebedev777 <naotlichnenko@gmail.com>
2019-07-22 00:25:15 +0300

    example for GaussianNB

---
+++ examples/example_naive_bayes_gaussian.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 21 21:10:53 2019
+
+@author: sl
+"""
+
+from sklearn import datasets
+from sklearn.metrics import classification_report
+from sklearn.naive_bayes import GaussianNB as skGNB
+from smlib.naive_bayes.nb import GaussianNB
+
+digits = datasets.load_digits()
+
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+X_train, y_train = data[:n_samples // 2], digits.target[:n_samples // 2]
+X_test, y_test   = data[n_samples // 2:], digits.target[n_samples // 2:]
+
+
+for classifier in [skGNB(), GaussianNB()]:
+    classifier.fit(X_train, y_train)
+   
+    print(classifier)
+    expected = y_test
+    predicted = classifier.predict(X_test)
+    print(classification_report(expected, predicted))
+    
